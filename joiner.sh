df57c418c581b7687f15e9a0dfdd4446187100bc
Ris-rajeev <146698975+Ris-rajeev@users.noreply.github.com>
2023-12-30 16:07:22 +0530

    Update joiner.sh

--- joiner.sh
+++ joiner.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Step 1: Fetch the private IP of the master node from the file saved during provisioning
-MASTER_PRIVATE_IP=$(cat master_private_ip.txt)  # Assuming the IP is saved in master_private_ip.txt
+MASTER_PRIVATE_IP=$(cat /home/$USER/master_private_ip.txt)  # Assuming the IP is saved in master_private_ip.txt
 echo $MASTER_PRIVATE_IP
 
-chmod 400 k8s-key-pair.pem
+chmod 400 /home/$USER/k8s-key-pair.pem
 
 # Step 2: SSH into the master node and copy kubeadm_join.sh to the worker
-scp -i k8s-key-pair.pem -o StrictHostKeyChecking=no  rajeev@$MASTER_PRIVATE_IP:/home/rajeev/kubeadm_join.sh kubeadm_join.sh
+scp -i /home/$USER/k8s-key-pair.pem -o StrictHostKeyChecking=no  $USER@$MASTER_PRIVATE_IP:/home/$USER/kubeadm_join.sh kubeadm_join.sh
 
 # Step 3: Modify the fetched kubeadm_join.sh script to include sudo before the join command
 sed -i 's/^/sudo /' kubeadm_join.sh
